7968b40ee9cb00a6387d5332c4452b6d4f1d794e
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2023-03-28 17:09:07 -0600

    feat: change spectre highlight colors

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,49 +1,61 @@
 return {
-  {
-    'stevearc/dressing.nvim',
-    lazy = true,
-    init = function()
-      vim.ui.select = function(...)
-        require('lazy').load { plugins = { 'dressing.nvim' } }
-        return vim.ui.select(...)
-      end
+	{
+		"stevearc/dressing.nvim",
+		lazy = true,
+		init = function()
+			vim.ui.select = function(...)
+				require("lazy").load({ plugins = { "dressing.nvim" } })
+				return vim.ui.select(...)
+			end
 
-      vim.ui.input = function(...)
-        require('lazy').load { plugins = { 'dressing.nvim' } }
-        return vim.ui.input(...)
-      end
-    end,
-  },
-  {
-    'windwp/nvim-spectre',
-    -- stylua: ignore
-    keys = {
-      { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
-    },
-  },
-  {
-    'nvim-zh/colorful-winsep.nvim',
-    opts = {
-      highlight = {
-        bg = '#16161E',
-        fg = '#1F3442',
-      },
-      -- timer refresh rate
-      interval = 30,
-    },
-    event = { 'WinNew' },
-  },
-  {
-    'anuvyklack/windows.nvim',
-    dependencies = {
-      'anuvyklack/middleclass',
-      'anuvyklack/animation.nvim',
-    },
-    opts = {
-      autowidth = { enable = true },
-      ignore = {
-        filetype = { 'neo-tree', 'lazy', 'noice' },
-      },
-    },
-  },
+			vim.ui.input = function(...)
+				require("lazy").load({ plugins = { "dressing.nvim" } })
+				return vim.ui.input(...)
+			end
+		end,
+	},
+	{
+		"windwp/nvim-spectre",
+		opts = {
+			highlight = {
+				ui = "String",
+				search = "IncSearch",
+				replace = "Substitute",
+			},
+		},
+		keys = {
+			{
+				"<leader>sr",
+				function()
+					require("spectre").open()
+				end,
+				desc = "Replace in files (Spectre)",
+			},
+		},
+	},
+	{
+		"nvim-zh/colorful-winsep.nvim",
+		opts = {
+			highlight = {
+				bg = "#16161E",
+				fg = "#1F3442",
+			},
+			-- timer refresh rate
+			interval = 30,
+		},
+		event = { "WinNew" },
+	},
+	{
+		"anuvyklack/windows.nvim",
+		dependencies = {
+			"anuvyklack/middleclass",
+			"anuvyklack/animation.nvim",
+		},
+		opts = {
+			autowidth = { enable = true },
+			ignore = {
+				filetype = { "neo-tree", "lazy", "noice" },
+			},
+		},
+	},
 }
